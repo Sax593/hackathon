25d5c58b04212acf8af8a781f02477dd702b4fdd
Priscillia <selvapriscillia@yahoo.fr>
2022-11-24 20:59:46 +0100

    modification puce table

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -9,87 +9,95 @@ import orion from "@assets/orion.jpg";
 
 export default function Table() {
   return (
-    <div className="container">
-      <div className="planets">
-        <h1 className="titleTable">Planets</h1>
-        <ul>
-          <li className="planet">
-            <img className="imgPlanet" src={deathStar} alt="Death Star" />
-            Death Star
-          </li>
-          <li className="planet">
-            <img className="imgPlanet" src={coruscant} alt="Coruscant" />
-            Coruscant
-          </li>
-          <li className="planet">
-            <img className="imgPlanet" src={cybertron} alt="Cybertron" />
-            Cybertron
-          </li>
-          <li className="planet">
-            <img className="imgPlanet" src={gargantua} alt="Gargantua" />
-            Gargantua
-          </li>
-          <li className="planet">
-            <img className="imgPlanet" src={krypton} alt="Krypton" />
-            Krypton
-          </li>
-          <li className="planet">
-            <img className="imgPlanet" src={mars} alt="Mars" />
-            Mars
-          </li>
-          <li className="planet">
-            <img className="imgPlanet" src={orion} alt="Orion" />
-            Orion
-          </li>
-        </ul>
-      </div>
-      <div className="solarFlare">
-        <h1 className="titleTable">Solar Flare</h1>
-        <ul>
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleRed" />
-          <li className="cercleGreen" />
-          <li className="cercleRed" />
-          <li className="cercleGreen" />
-        </ul>
-      </div>
-      <div className="supernova">
-        <h1 className="titleTable">Super Nova</h1>
-        <ul>
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleRed" />
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-        </ul>
-      </div>
-      <div className="asteroidImpact">
-        <h1 className="titleTable">Astéroïd Impact</h1>
-        <ul>
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-          <li className="cercleRed" />
-          <li className="cercleOrange" />
-          <li className="cercleGreen" />
-          <li className="cercleGreen" />
-        </ul>
-      </div>
-      <div className="flight">
-        <h1 className="titleTable">Flight</h1>
-        <ul>
-          <li className="list-flight" data-status="Confirmed" />
-          <li className="list-flight" data-status="Confirmed" />
-          <li className="list-flight" data-status="Confirmed" />
-          <li className="list-flight">hjfdsjkfh</li>
-          <li className="list-flight" data-status="Late" />
-          <li className="list-flight" data-status="Cancelled" />
-          <li className="list-flight" data-status="Confirmed" />
-        </ul>
+    <div className="mainContainer">
+      <h1 className="titleTable">Space Weather</h1>
+      <ul className="risk">
+        <li className="riskLow bulletGreen">Risk low</li>
+        <li className="riskMiddle bulletOrange">Risk middle</li>
+        <li className="riskHight bulletRed">Risk hight</li>
+      </ul>
+      <div className="container">
+        <div className="planets">
+          <h2 className="titleColumn">Planets</h2>
+          <ul>
+            <li className="planet">
+              <img className="imgPlanet" src={deathStar} alt="Death Star" />
+              Death Star
+            </li>
+            <li className="planet">
+              <img className="imgPlanet" src={coruscant} alt="Coruscant" />
+              Coruscant
+            </li>
+            <li className="planet">
+              <img className="imgPlanet" src={cybertron} alt="Cybertron" />
+              Cybertron
+            </li>
+            <li className="planet">
+              <img className="imgPlanet" src={gargantua} alt="Gargantua" />
+              Gargantua
+            </li>
+            <li className="planet">
+              <img className="imgPlanet" src={krypton} alt="Krypton" />
+              Krypton
+            </li>
+            <li className="planet">
+              <img className="imgPlanet" src={mars} alt="Mars" />
+              Mars
+            </li>
+            <li className="planet">
+              <img className="imgPlanet" src={orion} alt="Orion" />
+              Orion
+            </li>
+          </ul>
+        </div>
+        <div className="solarFlare">
+          <h2 className="titleColumn">Solar Flare</h2>
+          <ul>
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletRed" />
+            <li className="bulletGreen" />
+            <li className="bulletRed" />
+            <li className="bulletGreen" />
+          </ul>
+        </div>
+        <div className="supernova">
+          <h2 className="titleColumn">Super Nova</h2>
+          <ul>
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletRed" />
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+          </ul>
+        </div>
+        <div className="asteroidImpact">
+          <h2 className="titleColumn">Astéroïd Impact</h2>
+          <ul>
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+            <li className="bulletRed" />
+            <li className="bulletOrange" />
+            <li className="bulletGreen" />
+            <li className="bulletGreen" />
+          </ul>
+        </div>
+        <div className="flight">
+          <h2 className="titleColumn">Flight</h2>
+          <ul>
+            <li className="list-flight" data-status="Confirmed" />
+            <li className="list-flight" data-status="Confirmed" />
+            <li className="list-flight" data-status="Confirmed" />
+            <li className="list-flight">hjfdsjkfh</li>
+            <li className="list-flight" data-status="Late" />
+            <li className="list-flight" data-status="Cancelled" />
+            <li className="list-flight" data-status="Confirmed" />
+          </ul>
+        </div>
       </div>
     </div>
   );
