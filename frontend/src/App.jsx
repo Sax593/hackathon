a5855b246688b12be7f59c441f3d389e8782cfe0
sax59 <waquetv@gmail.com>
2022-11-24 23:46:12 +0100

    Add reset css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
-// import Home from "./pages/Home/Home";
 import Starmap from "@pages/Starmap/Starmap";
 
 function App() {
--- frontend/src/pages/Starmap/Starmap.jsx
+++ frontend/src/pages/Starmap/Starmap.jsx
@@ -1,4 +1,4 @@
-import Footer from "@components/Footer/Footer";
+import Footer from "@components/Footer";
 import Header from "@components/Header/Header";
 import Overlay from "@components/Overlay/Overlay";
 import RedAlert from "@components/Redalert";
