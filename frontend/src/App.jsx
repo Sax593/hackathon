17c82f48ba316438b341fd343de5d2c5aa025741
Fbouchezpro <fbouchezpro@gmail.com>
2022-11-24 13:57:17 +0100

    create red alert components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import Home from "./pages/Home";
-
+import RedAlert from "@components/Redalert";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <RedAlert />
     </div>
   );
 }
