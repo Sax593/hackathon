49e09f1e1e213da187a7c43fcb556606b0e5c5c8
Fbouchezpro <fbouchezpro@gmail.com>
2022-11-24 22:32:48 +0100

    adding the footer

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
-import PlanetCard from "@components/PlanetCard";
+import Footer from "@components/Footer";
+import Header from "@components/Header/Header";
+import RedAlert from "@components/Redalert";
 
 export default function Home() {
   return (
-    <div>
-      <PlanetCard />
+    <div className="background">
+      <Header />
+      <div className="main">
+        <RedAlert />
+      </div>
+      <Footer />
     </div>
   );
 }
