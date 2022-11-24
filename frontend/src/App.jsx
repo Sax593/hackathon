c3f6fc52631c6e35123428883c44575cf404d0b1
FlorianBartier <florianbartierpro@gmail.com>
2022-11-24 20:59:25 +0100

    Creation of the flight booking page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,23 @@ import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "@pages/Home/Home";
+import FlightBook from "@components/FlightBook/FlightBook";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <FlightBook />
+      {/* <Router>
         <div className="app">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/news" element={a} />
+            <Route path="/news" element={a} />
             <Route path="/starMap" element={a} />
             <Route path="/bookAFlight" element={a} />
-            <Route path="/spaceWeather" element={a} /> */}
+            <Route path="/spaceWeather" element={a} />
           </Routes>
         </div>
-      </Router>
+      </Router> */}
     </div>
   );
 }
