628ca2de6185e14db7c5800328a72e813251089a
Fbouchezpro <fbouchezpro@gmail.com>
2022-11-25 09:51:54 +0100

    adding intro animation

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-
 import DisplayHome from "@components/DisplayHome/DisplayHome";
 // import Header from "@components/Header/Header";
 // import PlanetCard from "@components/PlanetCard";
@@ -8,19 +7,23 @@ import Departure from "@components/departure";
 import Footer from "@components/Footer";
 import Header from "@components/Header/Header";
 import RedAlert from "@components/Redalert";
-
+import "./style.css";
 
 export default function Home() {
   return (
     <div className="background">
-    <DisplayHome />
-      <Header />
-      <div className="main">
-        <RedAlert />
-        <Departure />
+      <div className="fade-out">
+        <DisplayHome />
       </div>
-      <Booking />
-      <Footer />
+      <div className="fade-in">
+        <Header />
+        <div className="main">
+          <RedAlert />
+          <Departure />
+        </div>
+        <Booking />
+        <Footer />
+      </div>
     </div>
   );
 }
