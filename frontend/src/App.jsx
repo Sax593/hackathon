66525515ae3505bbb12002175a90322e7ebdf05a
Fbouchezpro <fbouchezpro@gmail.com>
2022-11-25 09:14:49 +0100

    fixes somes bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import "./reset.css";
 import "./App.css";
-
-
+import NewsPage from "@pages/NewsPage/NewsPage";
+import SpaceWeather from "@pages/SpaceWeather/SpaceWeather";
+import OtherStarmap from "@pages/OtherStarmap/OtherStarmap";
+import StantonStarmap from "@pages/StantonStarmap/StantonStarmap";
+import SolarStarmap from "@pages/SolarStarmap/SolarStarmap";
+import Home from "@pages/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className="app">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/starmap" element={<SolarStarmap />} />
-            <Route path="/starmap/solarsystem" element={<SolarStarmap />} />
-            <Route path="/starmap/stantonsystem" element={<StantonStarmap />} />
-            <Route path="/starmap/othersystem" element={<OtherStarmap />} />
-            <Route path="/spaceWeather" element={<SpaceWeather />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/bookAFlight" element={a} /> 
-            <Route path="/bookAFlight" element={a} /> */}
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/starmap" element={<SolarStarmap />} />
+          <Route path="/starmap/solarsystem" element={<SolarStarmap />} />
+          <Route path="/starmap/stantonsystem" element={<StantonStarmap />} />
+          <Route path="/starmap/othersystem" element={<OtherStarmap />} />
+          <Route path="/spaceWeather" element={<SpaceWeather />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/flightbook" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
